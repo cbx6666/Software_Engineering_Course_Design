2fac08915f32f40de69859c48ba8cf3a6acdd8ed
Wing_WR <wingwr998@gmail.com>
2026-01-07 07:17:33 +0800

    fix: 修改结果的描述样式和文字一样

--- front-end/src/components/detection/DetailsList.tsx
+++ front-end/src/components/detection/DetailsList.tsx
@@ -17,7 +17,7 @@ export function DetailsList({ details }: { details: DetectionDetail[] }) {
                             {detail.description && (
                                 <Popover>
                                     <PopoverTrigger asChild>
-                                        <button className="text-white hover:text-slate-400 transition-colors focus:outline-none">
+                                        <button className="text-slate-200 hover:text-white transition-colors focus:outline-none">
                                             <HelpCircle className="w-4 h-4" />
                                         </button>
                                     </PopoverTrigger>
